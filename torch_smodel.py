4c0f3db8321ae45bf47fcd9f919cfbf23dcd3d44
Joan Boters <joanboterspitarch98@gmail.com>
2022-12-19 23:27:13 +0100

    REFACTORING THE CODE

--- torch_smodel.py
+++ torch_smodel.py
@@ -53,27 +53,31 @@ def FunDiag(theta):
     elif  np.pi/4 < aux < np.pi/2:
         return 1/torch.tan(torch.Tensor([aux]))
 
-def RotacionIzquierda(array):
-    """ rotates to the left of our array
-    Args:
-        array (array): array which will be rotated
-    Returns:
-        array: we get our rotated array
-    """
-    new_array = torch.rot90(array, k=1, dims = [0,1])
-    #new_array = torch.t(array)
-    return new_array
+# def RotacionIzquierda(array):
+#    """ rotates to the left of our array
+#    Args:
+#        array (array): array which will be rotated
+#    Returns:
+#        array: we get our rotated array
+#    """
+#    return torch.rot90(array, k=1)
+#    new_array = torch.t(array)
+#    return new_array
 
-def RotacionDerecha(array):
-    """ rotates to the right of our array
-    Args:
-        array (array): array which will be rotated
-    Returns:
-        array: we get our rotated array
-    """
-    new_array = torch.rot90(array, k=-1)
-    #new_array = torch.t(array)
-    return new_array
+#def RotacionDerecha(array):
+#    """ rotates to the right of our array
+#    Args:
+#        array (array): array which will be rotated
+#    Returns:
+#        array: we get our rotated array
+#    """
+#    new_array = torch.rot90(array, k=-1)
+#    new_array = torch.t(array)
+#    return new_array
+
+#RotacionDerecha = torch.rot90
+
+#RotacionDerecha(array, k=-1) == torch.rot90(array, k=-1)
 
 def Rotacion(array, theta):
     """ this function will rotate our array depending on theta
@@ -86,11 +90,11 @@ def Rotacion(array, theta):
     if Q(theta)==1:
         return array
     elif Q(theta)==2:
-        return torch.t(RotacionIzquierda(array))
+        return torch.t(torch.rot90(array, k=1))
     elif Q(theta)==3:
         return torch.rot90(array, k=2)
     elif Q(theta)==4:
-        return torch.t(RotacionDerecha(array))
+        return torch.t(torch.rot90(array, k=-1))
 
 def init (N, K):
     """Initialize our arrays in order to start the model.
@@ -108,8 +112,9 @@ def init (N, K):
 
 class Grid:
 
-    def __init__ (self, N):
+    def __init__ (self, N, K):
         self.N = N
+        self.K=K
         self.n = int(N/2)
         self.state = torch.zeros(self.N, self.N, dtype = torch.int8)
         self.state[self.n, self.n] = 1
@@ -120,12 +125,11 @@ class Grid:
         self.deads = 0
         self.neigh_prob = torch.zeros(self.N, self.N, dtype = torch.float64)
         self.m = 0
-        self.columnas = ['Theta', 'Quadrant', 'Rho', 'm', 'Susceptibles', 'Infecteds', 'Deads']
-        self.df = pd.DataFrame([[None, None, None, None, self.susceptibles, self.infecteds, self.deads]], columns = self.columnas)
+        self.columnas = ['Theta', 'Quadrant', 'Xi', 'Rho', 'm', 'Susceptibles', 'Infecteds', 'Deads']
+        self.df = pd.DataFrame(index = range(self.K + 1), columns = self.columnas)
+        self.df.iloc[0] = [0, 0, 0, 0, 0, self.susceptibles, self.infecteds, self.deads]
     
-    def __param__(self, K=10, inc=1, partition=[0.1, 0.5, 0.9], p0=0.25, div = 2):
-        
-        self.K = K
+    def __param__(self, inc=1, partition=[0.1, 0.5, 0.9], p0=0.25, div = 2): 
         self.inc = inc
         self.partition = partition
         self.div = div
@@ -213,10 +217,11 @@ class Grid:
         self.infecteds = torch.sum(self.state==1).item()
         self.deads = torch.sum(self.state==2).item()
     
-    def write_df(self, theta, rho):
-        df_aux = pd.DataFrame([[theta, Q(theta), rho, self.exp, self.susceptibles, self.infecteds, self.deads]], columns = self.columnas)
-        self.df = pd.concat([self.df, df_aux], ignore_index=True)
-    
+    def write_df(self, step):
+        self.df.Susceptibles[step] = self.susceptibles
+        self.df.Infecteds[step] = self.infecteds
+        self.df.Deads[step] = self.deads
+        
     def Expansion(self, seed_value=2022, input=False, data=None, tau=1):
         
         torch.random.manual_seed(seed_value)
@@ -225,16 +230,28 @@ class Grid:
         if self.N%2 == 0:
             N += 1
         if input == False:
-            Theta = np.random.uniform(low=0, high=2*np.pi, size=self.K)
-            Rho = np.random.uniform(low=0, high=1, size=self.K)
+            Theta = torch.from_numpy(np.random.uniform(low=0, high=2*np.pi, size=self.K))
+            Rho = torch.from_numpy(np.random.uniform(low=0, high=1, size=self.K))
         else:
-            Theta = list(data.Theta)
-            Rho = list(data.Rho)
+            Theta = torch.Tensor(data.Theta.values.astype('float64'))
+            Rho = torch.Tensor(data.Rho.values.astype('float64'))
         
         self.S = torch.zeros(self.N, self.N, self.K+1, dtype=torch.int8)
         self.P = torch.zeros(self.N, self.N, self.K, dtype=torch.float64)
         self.S[:, :, 0] = self.state.clone()
-        self.__param__(K=self.K, inc=self.inc, partition=self.partition, p0=self.p0, div=self.div)
+        self.__param__(inc=self.inc, partition=self.partition, p0=self.p0, div=self.div)
+
+        # adding columns Theta, Q, Xi, Rho, m
+        self.df.Theta[1:] = Theta
+        q = torch.from_numpy(np.array(Theta/(np.pi/2) + 1, dtype='int8'))
+        self.df.Quadrant[1:] = q
+        self.df.Xi[1:] = torch.where(
+            torch.logical_or(q == 1, q == 3),
+            Theta - ((q-1)/2)*torch.pi,
+            (q/2)*torch.pi - Theta
+        )
+        self.df.Rho[1:] = Rho
+        #self.df.m[1:] = 
 
         for L in range(self.K):
             theta, rho = Theta[L], Rho[L]
@@ -244,7 +261,7 @@ class Grid:
             self.P[:, :, L] = self.neigh_prob.clone()
             self.update(inc=self.inc, tau=tau)
             self.S[:, :, L+1] = self.state.clone()
-            self.write_df(theta=theta, rho=rho)
+            self.write_df(step=L+1)
 
     def MonteCarlo(self, n_it = 10**3, input=False, data=None):
 
@@ -261,7 +278,7 @@ class Grid:
             self.dataMC = self.df[['Theta', 'Rho']].drop(0).copy()
                         
         for seed in range(n_it):
-            self.__init__(N=self.N)
+            self.__init__(N=self.N, K=self.K)
             self.__param__()
             self.Expansion(seed_value=seed, input=True, data=self.dataMC)
             self.X0 += (self.S==0)*1.
@@ -288,9 +305,9 @@ def SpreadModel(seed_value=2022, N=7, K=5, inc=1, partition=[0.1, 0.5, 0.9], p0=
         Theta = list(data.Theta)
         Rho = list(data.Rho)
     E, P = init(N=N, K=K)
-    grid = Grid(N=N)
+    grid = Grid(N=N, K=K)
     E[:, :, 0] = grid.state.clone()
-    grid.__param__(K=K, inc=inc, partition=partition, p0=p0, div=div)
+    grid.__param__(inc=inc, partition=partition, p0=p0, div=div)
 
     for L in range(K):
         theta, rho = Theta[L], Rho[L]
@@ -298,7 +315,7 @@ def SpreadModel(seed_value=2022, N=7, K=5, inc=1, partition=[0.1, 0.5, 0.9], p0=
         grid.enlargement_process(theta=theta, rho=rho)
         grid.neighbourhood_relation()
         grid.update(inc=inc, tau=tau)
-        grid.write_df(theta=theta, rho=rho)
+        grid.write_df(step=L+1)
         E[:, :, L+1] = grid.state.clone()
         P[:, :, L] = grid.neigh_prob.clone()    
     return E, P, grid.df
