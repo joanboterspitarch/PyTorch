a728085aa0ec66af88ac30da755771ab41f8f387
Joan Boters <joanboterspitarch98@gmail.com>
2023-01-04 17:59:26 +0100

    Hemos creado la clase AI_Grid para usar la IA

--- torch_smodel.py
+++ torch_smodel.py
@@ -6,6 +6,39 @@ import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
 import torch.nn.functional as F
 
+class ArchNN(torch.nn.Module):
+    
+        def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size_p, output_size_c):
+
+            super(ArchNN, self).__init__()
+            self.input_size = input_size
+            self.hidden_size_1 = hidden_size_1
+            self.hidden_size_2 = hidden_size_2
+            self.output_size_p = output_size_p
+            self.output_size_c = output_size_c
+            self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size_1)
+            self.fc2 = torch.nn.Linear(self.hidden_size_1, self.hidden_size_2)
+            self.fc3 = torch.nn.Linear(self.hidden_size_2, self.output_size_p)
+            self.fc4 = torch.nn.Linear(self.hidden_size_2, self.output_size_c)
+            self.relu = torch.nn.ReLU()
+            self.sigmoid = torch.nn.Sigmoid()
+            self.linear = torch.nn.Linear(self.output_size_c, 1)
+    
+        def forward(self, x):
+            
+            x = self.fc1(x)
+            x = self.relu(x)
+            x = self.fc2(x)
+            x = self.relu(x)
+
+            x1 = self.fc3(x)
+            x1 = self.sigmoid(x1)
+
+            x2 = self.fc4(x)
+            x2 = self.linear(x2)
+
+            return x1, x2
+
 class Grid:
 
     """
@@ -393,7 +426,9 @@ class Grid:
         ).type(torch.int8) 
 
         # Las matrices del enlargement process están calculadas.
-
+        
+        #self.submatrix()
+        #self.enlargement_process()
         large_matrices = self.large_matrix.clone()
 
         for seed in range(n_it):
@@ -434,4 +469,193 @@ class Grid:
         self.X1 = self.X1/n_it
         self.X2 = self.X2/n_it
         self.df_MC = self.df_MC/n_it
-        #self.df_MC[['Theta', 'Rho', 'Quadrant', 'Xi', 'm']] = aux
+
+
+class AI_Grid (Grid):
+
+    def __init__(self, N, K):
+            
+        super(AI_Grid, self).__init__(N=N, K=K)
+
+    def __param__(self, inc, partition):
+        
+        self.inc = inc
+        self.partition = partition
+    
+    def enlargement_process(self):
+        
+        # creating matrices
+        
+        ## 1. When is between u_2, u_3
+        M2 = torch.zeros(size=(5, 5, self.K), dtype=torch.float64)
+        M2[1:-1, 1:-1, :] = self.A
+        M2[0, 2, :] = M2[1, 2, :]/self.div
+        M2[0, 4, :] = M2[1, 3, :]/self.div
+        M2[2, 4, :] = M2[2, 3, :]/self.div
+        M2[0, 3, :] = (M2[0, 2, :] + M2[0, 4, :])/2
+        M2[1, 4, :] = (M2[0, 4, :] + M2[2, 4, :])/2
+
+        ## 2. When is between u_3, 1
+        M3 = torch.zeros(size=(7, 7, self.K), dtype=torch.float64)
+        M3[1:-1, 1:-1, :] = M2
+        M3[0, 3, :] = M3[1, 3, :]/self.div
+        M3[0, 4, :] = M3[1, 4, :]/self.div
+        M3[0, 6, :] = M3[1, 5, :]/self.div
+        M3[0, 5, :] = (M3[0, 4, :] + M3[0, 6, :])/2
+        M3[2, 6, :] = M3[2, 5, :]/self.div
+        M3[3, 6, :] = M3[3, 5, :]/self.div
+        M3[1, 6, :] = (M3[0, 6, :] + M3[2, 6, :])/2
+
+        # Final tensor after enlargement process
+        self.large_matrix = torch.zeros(size=(7, 7, self.K), dtype=torch.float64)
+
+        self.large_matrix[2:5, 2:5, self.m==0] = self.p0[self.m==0]
+        self.large_matrix[3, 3, self.m==0] = 0
+        self.large_matrix[2:5, 2:5, self.m==1] = self.A[:, :, self.m==1]
+        self.large_matrix[1:-1, 1:-1, self.m==2] = M2[:, :, self.m==2]
+        self.large_matrix[:, :, self.m==3] = M3[:, :, self.m==3]
+        
+        self.large_matrix[:, :, self.q == 2] = torch.transpose(
+            torch.rot90(
+                self.large_matrix[:, :, self.q == 2],
+                k=1
+            ),
+            0,
+            1
+        )
+
+        self.large_matrix[:, :, self.q == 3] = torch.rot90(
+            self.large_matrix[:, :, self.q == 3],
+            k = 2
+        )
+
+        self.large_matrix[:, :, self.q == 4] = torch.transpose(
+            torch.rot90(
+                self.large_matrix[:, :, self.q == 4],
+                k=-1
+            ),
+            0,
+            1
+        )
+    
+    def load_data(self, data):
+        
+        self.data = data
+        self.Theta = torch.Tensor(self.data.Theta.to_numpy())
+        self.Rho = torch.Tensor(self.data.Rho.to_numpy())
+        self.Hum = torch.Tensor(self.data.Humidity.to_numpy())
+        self.Tem = torch.Tensor(self.data.Temperature.to_numpy())
+
+        self.q = (self.Theta/(torch.pi/2) + 1).type(torch.int8)
+        self.xi = torch.where(
+            torch.logical_or(self.q == 1, self.q == 3),
+            self.Theta - ((self.q-1)/2)*torch.pi,
+            (self.q/2)*torch.pi - self.Theta
+        )
+        self.m = torch.where(
+            self.Rho <= self.partition[1],
+            torch.where(
+                self.Rho <= self.partition[0],
+                0,
+                1
+            ),
+            torch.where(
+                self.Rho <= self.partition[2],
+                2,
+                3
+            )
+        ).type(torch.int8)
+
+        self.df_nn = self.data.drop(columns=['Theta', 'Rho'])
+
+    def AI_MonteCarlo(self, input_size, target_s, target_d, n_it=10**3, tau=1, epochs=100):
+
+        self.model = ArchNN(
+            input_size=input_size,
+            hidden_size_1= 10,
+            hidden_size_2=5,
+            output_size_p=3,
+            output_size_c=3
+        )
+
+        log_each = 10
+
+        l = []
+
+        crit = torch.nn.MSELoss()
+        opt = torch.optim.SGD(self.model.parameters(), lr=0.5)
+
+        self.model.train()
+
+        for epoch in epochs:
+
+            self.p0, self.div = self.model(self.df_nn)
+
+            self.X0 = torch.zeros(self.N, self.N, self.K + 1, dtype=torch.float64)
+            self.X1 = torch.zeros(self.N, self.N, self.K + 1, dtype=torch.float64)
+            self.X2 = torch.zeros(self.N, self.N, self.K + 1, dtype=torch.float64)
+
+            self.submatrix()
+            self.enlargement_process()
+            large_matrices = self.large_matrix.clone()
+
+            for s in range(n_it):
+
+                torch.random.manual_seed(s)
+                np.random.seed(s)
+
+                self.__init__(N=self.N, K=self.K)
+
+                self.S = torch.zeros(self.N, self.N, self.K+1, dtype=torch.int8)
+                self.P = torch.zeros(self.N, self.N, self.K, dtype=torch.float64)
+                self.S[:, :, 0] = self.state.clone()
+                padding = torch.zeros(self.N + 6, self.N + 6, dtype=torch.float64)
+
+                for L in range(self.K):
+
+                    padding = torch.zeros(self.N + 6, self.N + 6, dtype=torch.float64)
+                    for i,j in self.ind:
+                        padding[i:(i+7), j:(j+7)] += large_matrices[:, :, L]
+                    
+                    self.neigh_prob = padding[3:-3, 3:-3].clone()
+                    self.P[:, :, L] = self.neigh_prob.clone()
+                    self.update(tau=tau)
+                    self.S[:, :, L+1] = self.state.clone()
+                
+                self.X0 += (self.S==0)*1.
+                self.X1 += (self.S==1)*1.
+                self.X2 += (self.S==2)*1.
+
+            self.X0 /= n_it
+            self.X1 /= n_it
+            self.X2 /= n_it
+
+            l.append(loss.item())
+
+            opt.zero_grad()
+
+            l1 = crit(self.X0, target_s)
+            l2 = crit(self.X2, target_d)
+
+            loss = l1 + l2
+
+            loss.backward()
+
+            opt.step()
+            
+            if not epoch % log_each:
+                lr = 0.5 - (0.4*epoch/epochs)
+                tau = 1 - (0.3*epoch/epochs)
+                print(f'Epoch {epoch}/{epochs} Loss {np.mean(l):.5f} Tau {tau} Learn Rate {lr}')
+                opt = torch.optim.SGD(self.model.parameters(), lr=lr)
+
+
+
+
+
+
+
+
+
+
+
